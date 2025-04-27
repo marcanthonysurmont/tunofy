6ffb155ed11efabd7b27f3b51b64a728213d38f2
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-04-27 22:20:34 +0200

    added fonts + some tailwind color variables

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,10 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tunofy - Create.Vote.Listen</title>
     @vite(['resources/js/landingpage.js'])
+    {{-- fonts for headers --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
 </head>
 
-<body>
-    <h1>lol</h1>
+<body class="bg-background-page">
+    <h1 class="text-3xl font-nohemi">Nohemi</h1>
+    <h1 class="text-3xl font-nunito">Nunito</h1>
 </body>
 
 </html>
