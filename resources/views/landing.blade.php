8ca6d65d2f9824cb71f972f39f54f56fe5a69ca4
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-04-28 00:34:13 +0200

    feat: added navbar open/close hamburger animation

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -41,12 +41,19 @@
                 </div>
 
                 <!-- Mobile hamburger button -->
-                <button @click="open = !open" class="lg:hidden text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
+                <button @click="open = !open"
+                    class="lg:hidden text-white m-2 relative flex items-center justify-center">
+                    <div class="relative w-6 h-6">
+                        <!-- Line 1 -->
+                        <span class="absolute h-0.5 w-6 bg-current rounded-full transition-all duration-300"
+                            :class="open ? 'rotate-45 top-3' : 'top-1'"></span>
+                        <!-- Line 2 -->
+                        <span class="absolute h-0.5 w-6 bg-current rounded-full top-3 transition-all duration-300"
+                            :class="open ? 'opacity-0' : 'opacity-100'"></span>
+                        <!-- Line 3 -->
+                        <span class="absolute h-0.5 w-6 bg-current rounded-full transition-all duration-300"
+                            :class="open ? '-rotate-45 top-3' : 'top-5'"></span>
+                    </div>
                 </button>
             </div>
 
