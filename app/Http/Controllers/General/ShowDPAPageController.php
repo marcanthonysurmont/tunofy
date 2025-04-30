e8ecff9c8acb87fe897c39fddaded770fbfbe958
marcanthonysurmont <147101133+marcanthonysurmont@users.noreply.github.com>
2025-04-30 22:15:04 +0200

    Refactor ShowDPAPageController to remove unused Request

--- app/Http/Controllers/General/ShowDPAPageController.php
+++ app/Http/Controllers/General/ShowDPAPageController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers\General;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
 
 class ShowDPAPageController extends Controller
 {
-    public function __invoke(Request $request): View
+    public function __invoke(): View
     {
         return view('dpa');
     }
