da8cc0f45cb8a8623ce179742e23fdf09fa01fe4
marcanthonysurmont <147101133+marcanthonysurmont@users.noreply.github.com>
2025-05-01 13:19:49 +0200

    Add migrations, models, and seeders for presets, mixes, songs, votes, and queue songs

---
+++ database/migrations/2025_05_01_120046_create_mixes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mixes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('session_code')->nullable();
+            $table->boolean('is_public')->default(false);
+            $table->boolean('is_active')->default(false);
+            $table->foreignId('co_dj_id')->nullable()->constrained('users')->onDelete('cascade')->nullable();
+            $table->string('playback_device_id')->nullable();
+            $table->integer('batch_size')->default(10);
+            $table->integer('max_songs')->nullable();
+            $table->foreignId('preset_id')->nullable()->onDelete('cascade')->nullable();
+            $table->string('avatar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mixes');
+    }
+};
---
+++ database/migrations/2025_05_01_122048_create_songs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mix_id')->constrained()->onDelete('cascade');
+            $table->string('spotify_id')->index();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('duration_ms')->nullable();
+            $table->integer('priority_boost')->default(0);
+            $table->integer('vote_score')->default(0);
+            $table->boolean('is_killed')->default(false);
+            $table->dateTime('last_fetched_at')->nullable();
+            $table->string('name');
+            $table->string('artist');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
