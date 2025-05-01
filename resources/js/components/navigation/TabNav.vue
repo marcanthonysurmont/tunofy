3d030fe4fb413627ef943ef7de24203e50a71690
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-01 13:09:11 +0200

    fix: better rounded edges

--- resources/js/components/navigation/TabNav.vue
+++ resources/js/components/navigation/TabNav.vue
@@ -2,7 +2,7 @@
     <div class="mb-6">
         <div class="grid grid-cols-1 sm:hidden">
             <select aria-label="Select a tab"
-                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-2 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                class="col-start-1 row-start-1 w-full appearance-none rounded-lg bg-white py-2 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
                 @change="changeTab($event)">
                 <option v-for="tab in tabs" :key="tab.name" :selected="tab.active">{{ tab.name }}</option>
             </select>
@@ -13,7 +13,7 @@
         <div class="hidden sm:block">
             <nav class="flex space-x-4" aria-label="Tabs">
                 <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.active ? 'bg-primary text-white' : 'text-white hover:text-neutral-200 bg-tab-background-inactive border-2 border-tab-stroke-inactive',
-                    'rounded-md flex items-center px-3 py-2 text-3xl font-medium font-headers cursor-pointer']"
+                    'rounded-lg flex items-center px-3 py-2 text-3xl font-medium font-headers cursor-pointer']"
                     :aria-current="tab.active ? 'page' : undefined" @click.prevent="changeTab(tab.name)">
                     {{ tab.name }}
                 </a>
