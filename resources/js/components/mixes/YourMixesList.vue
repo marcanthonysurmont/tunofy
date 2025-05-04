2c76a955cf48e9465449331b1474f6e78b9d9541
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-04 23:38:52 +0200

    feat: active page is now highlighted in the navbar

--- resources/js/components/mixes/YourMixesList.vue
+++ resources/js/components/mixes/YourMixesList.vue
@@ -1,6 +1,10 @@
 <template>
     <ul v-for="mix in mixes" v-if="mixes.length > 0">
-        <YourMixesListItem :key="mix.id" :mix="mix"> </YourMixesListItem>
+        <YourMixesListItem
+            :key="mix.id"
+            :mix="mix"
+            :is-active="isActive(mix)"
+        />
     </ul>
     <div v-else>
         <p class="text-zinc-400">You don't have any mixes yet.</p>
@@ -15,4 +19,9 @@ import YourMixesListItem from "./YourMixesListItem.vue";
 const page = usePage();
 const props = computed(() => page.props);
 const mixes = computed(() => props.value.your_mixes);
+const currentMix = computed(() => props.value.mix);
+
+function isActive(mix) {
+    return currentMix.value && currentMix.value.id === mix.id;
+}
 </script>
