103b8fc751b7f40682ce5c63d3b8f88479b7056d
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-05 01:52:13 +0200

    feat: added disabled state and state tracking in the toggle switch component

--- resources/js/components/forms/ToggleSwitchDescription.vue
+++ resources/js/components/forms/ToggleSwitchDescription.vue
@@ -2,15 +2,25 @@
     <SwitchGroup as="div" class="flex items-center">
         <Switch
             v-model="enabled"
+            :disabled="disabled"
             :class="[
-                enabled ? 'bg-primary' : 'bg-zinc-700',
+                disabled
+                    ? 'cursor-not-allowed bg-zinc-400'
+                    : enabled
+                    ? 'bg-primary'
+                    : 'bg-zinc-700',
                 'relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:outline-hidden',
             ]"
+            @change="updateModelValue"
         >
             <span
                 aria-hidden="true"
                 :class="[
-                    enabled ? 'translate-x-5' : 'translate-x-0',
+                    disabled
+                        ? 'bg-zinc-300'
+                        : enabled
+                        ? 'translate-x-5'
+                        : 'translate-x-0',
                     'pointer-events-none inline-block size-5 transform rounded-full bg-white shadow-sm ring-0 transition duration-200 ease-in-out',
                 ]"
             />
@@ -23,10 +33,9 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { Switch, SwitchGroup, SwitchLabel } from "@headlessui/vue";
 
-// Props
 const props = defineProps({
     label: {
         type: String,
@@ -36,12 +45,26 @@ const props = defineProps({
         type: String,
         required: false,
     },
-    initialState: {
+    modelValue: {
+        type: Boolean,
+        default: false,
+    },
+    disabled: {
         type: Boolean,
         default: false,
     },
 });
 
-// Refs
-const enabled = ref(props.initialState);
+const enabled = ref(props.modelValue);
+
+watch(
+    () => props.modelValue,
+    (newValue) => {
+        enabled.value = newValue;
+    }
+);
+
+const updateModelValue = () => {
+    emit("update:modelValue", enabled.value);
+};
 </script>
