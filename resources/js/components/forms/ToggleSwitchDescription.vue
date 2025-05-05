e7d31ac0b4272dd595061138d33b1c7fa6f3bcef
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-05 02:11:03 +0200

    feat: disabled toggle now properly shows enabled/disabled state

--- resources/js/components/forms/ToggleSwitchDescription.vue
+++ resources/js/components/forms/ToggleSwitchDescription.vue
@@ -4,23 +4,16 @@
             v-model="enabled"
             :disabled="disabled"
             :class="[
-                disabled
-                    ? 'cursor-not-allowed bg-zinc-400'
-                    : enabled
-                    ? 'bg-primary'
-                    : 'bg-zinc-700',
-                'relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:outline-hidden',
+                enabled ? 'bg-primary' : 'bg-zinc-700',
+                disabled && 'opacity-50 cursor-not-allowed',
+                'relative inline-flex h-6 w-11 shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:outline-hidden',
             ]"
             @change="updateModelValue"
         >
             <span
                 aria-hidden="true"
                 :class="[
-                    disabled
-                        ? 'bg-zinc-300'
-                        : enabled
-                        ? 'translate-x-5'
-                        : 'translate-x-0',
+                    enabled ? 'translate-x-5' : 'translate-x-0',
                     'pointer-events-none inline-block size-5 transform rounded-full bg-white shadow-sm ring-0 transition duration-200 ease-in-out',
                 ]"
             />
