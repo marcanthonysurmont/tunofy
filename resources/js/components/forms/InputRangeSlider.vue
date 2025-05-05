6018bd6935f96f21a410863aebbe86d1a5a0b1a3
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-05 20:18:21 +0200

    fix: added disabled state to the input range slider

--- resources/js/components/forms/InputRangeSlider.vue
+++ resources/js/components/forms/InputRangeSlider.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full">
+    <div class="w-full" :class="{ 'opacity-40 cursor-not-allowed': disabled }">
         <label v-if="label" class="block text-sm font-medium text-white mb-4">
             {{ label }}
         </label>
@@ -12,6 +12,7 @@
 
         <input
             type="range"
+            :class="disabled ? 'cursor-not-allowed' : ''"
             class="w-full appearance-none bg-zinc-700 h-1 rounded-lg outline-none transition-all duration-200 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:shadow [&::-webkit-slider-thumb]:hover:bg-gray-200 [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-white"
             :min="min"
             :max="max"
