ca5cf1b5e2e4a742847ff9d4072552bdb33961e1
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-09 01:02:25 +0200

    feat: if voting is inactive, tab is disabled and vice versa

--- resources/js/components/navigation/TabNav.vue
+++ resources/js/components/navigation/TabNav.vue
@@ -25,15 +25,27 @@
                             }
                         "
                         :class="[
-                            'border-2 rounded-lg flex items-center justify-center px-3 pb-1.5 pt-2.5 tab-nav-item-center sm:text-xl md:text-2xl font-medium font-headers cursor-pointer hover:text-neutral-300 z-10 transition-colors duration-300 ease-in-out relative whitespace-nowrap',
+                            'border-2 rounded-lg flex items-center justify-center px-3 pb-1.5 pt-2.5 tab-nav-item-center sm:text-xl md:text-2xl font-medium font-headers hover:text-neutral-300 z-10 transition-colors duration-300 ease-in-out relative whitespace-nowrap',
                             tab.active
                                 ? 'border-primary text-white'
                                 : 'border-tab-stroke-inactive text-white',
+                            tab.votingActive === false && !tab.active
+                                ? 'opacity-50 cursor-not-allowed'
+                                : 'cursor-pointer',
                         ]"
                         :aria-current="tab.active ? 'page' : undefined"
                         @click.prevent="handleTabClick(tab.name, index)"
+                        :disabled="tab.votingActive === false"
                     >
                         {{ tab.name }}
+                        <span
+                            class="absolute -top-1 -right-1"
+                            v-if="tab.votingActive"
+                        >
+                            <ExclamationCircleIcon
+                                class="size-6 text-yellow-500"
+                            />
+                        </span>
                     </a>
                 </nav>
             </div>
@@ -46,6 +58,7 @@
 import { ref, onMounted, watch, computed, nextTick, onUnmounted } from "vue";
 import SearchBarSong from "@/components/SearchBarSong.vue";
 import { usePage } from "@inertiajs/vue3";
+import { ExclamationCircleIcon } from "@heroicons/vue/24/solid";
 
 const props = defineProps({
     tabs: {
@@ -71,6 +84,9 @@ function isActiveTab(index) {
 }
 
 function handleTabClick(tabName, index) {
+    if (tabName === "Voting" && !props.tabs[index].votingActive) {
+        return;
+    }
     emit("tab-changed", tabName);
     scrollToTab(index);
 
