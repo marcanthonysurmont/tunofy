74b87a15b91a3fd13ec60d921227641090007d7c
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-08 02:34:32 +0200

    fix: search input field accidental border

--- resources/js/components/SearchBarSong.vue
+++ resources/js/components/SearchBarSong.vue
@@ -19,7 +19,7 @@
                         aria-hidden="true"
                     />
                     <ComboboxInput
-                        class="w-full bg-transparent border-2 border-card-stroke text-dark-white placeholder-zinc-400 pl-4 pr-12 py-2 focus:outline-none"
+                        class="w-full bg-transparent text-dark-white placeholder-zinc-400 pl-4 pr-12 py-2 focus:outline-none"
                         @input="handleSearch"
                         placeholder="Search songs..."
                         autocomplete="off"
