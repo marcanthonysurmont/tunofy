bf64d0d415e5b7cc02d260774318b33933407a05
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-08 02:22:35 +0200

    feat: added headless ui float to the menu dropdown component
    this makes it work on very small screens

--- resources/js/components/menus/MenuDropdown.vue
+++ resources/js/components/menus/MenuDropdown.vue
@@ -1,33 +1,34 @@
 <template>
-    <Menu as="div" class="relative inline-block text-left z-[100]">
+    <Menu as="div" class="relative inline-block text-left">
         <div>
-            <MenuButton
-                class="inline-flex w-full justify-center rounded-md text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 cursor-pointer"
+            <Float
+                flip
+                enter="transition duration-100 ease-out"
+                enter-from="transform scale-95 opacity-0"
+                enter-to="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-in"
+                leave-from="transform scale-100 opacity-100"
+                leave-to="transform scale-95 opacity-0"
             >
-                <EllipsisHorizontalIcon class="size-9" />
-            </MenuButton>
+                <MenuButton
+                    class="inline-flex w-full z-10 justify-center rounded-md text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 cursor-pointer"
+                >
+                    <EllipsisHorizontalIcon class="size-9" />
+                </MenuButton>
+                <MenuItems
+                    class="shadow-2xl z-[10000] mt-2 w-56 xs:max-h-36 md:max-h-64 overflow-y-auto custom-scrollbar origin-top-right divide-y divide-gray-100 rounded-md bg-card-background border-2 border-card-stroke ring-1 ring-black/5 focus:outline-none"
+                >
+                    <div class="px-1.5 py-1.5">
+                        <slot></slot>
+                    </div>
+                </MenuItems>
+            </Float>
         </div>
-
-        <transition
-            enter-active-class="transition duration-100 ease-out"
-            enter-from-class="transform scale-95 opacity-0"
-            enter-to-class="transform scale-100 opacity-100"
-            leave-active-class="transition duration-75 ease-in"
-            leave-from-class="transform scale-100 opacity-100"
-            leave-to-class="transform scale-95 opacity-0"
-        >
-            <MenuItems
-                class="shadow-2xl z-[1000] absolute sm:left-0 right-0 mt-2 w-56 xs:max-h-36 md:max-h-64 overflow-y-auto custom-scrollbar origin-top-right divide-y divide-gray-100 rounded-md bg-card-background border-2 border-card-stroke ring-1 ring-black/5 focus:outline-none"
-            >
-                <div class="px-1.5 py-1.5">
-                    <slot></slot>
-                </div>
-            </MenuItems>
-        </transition>
     </Menu>
 </template>
 
 <script setup>
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 import { EllipsisHorizontalIcon } from "@heroicons/vue/24/outline";
+import { Float } from "@headlessui-float/vue";
 </script>
