5a1a88c659eb8ff89101cc498f2bfa61162ef30a
marcanthonysurmont <147101133+marcanthonysurmont@users.noreply.github.com>
2025-05-09 16:28:33 +0200

    Update exception class reference in DestroyMixController

--- app/Http/Controllers/Application/Mixes/DestroyMixController.php
+++ app/Http/Controllers/Application/Mixes/DestroyMixController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Application\Mixes;
 use App\Models\Mix;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Exception;
 
 class DestroyMixController extends Controller
 {
@@ -25,7 +25,7 @@ class DestroyMixController extends Controller
 
             return redirect()->route('app')
                 ->with('success', 'Mix deleted successfully.');
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return redirect()->back()
                 ->with('danger', 'Failed to delete mix. Please try again later.');
         }
