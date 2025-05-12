43680eacf350f4e66a800b88682f4214e23bfd5e
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-13 00:57:24 +0200

    feat: optimize app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,14 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
-    {{-- fonts for headers --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
         rel="stylesheet">
+    {{-- <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet"> --}}
+    @vite('resources/js/app.js')
     <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
     <link rel="shortcut icon" href="/favicons/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
@@ -18,11 +20,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @inertiaHead
     @routes
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-        rel="stylesheet">
 </head>
 
 <body class="bg-background-page text-dark-white mx-auto h-full" id="body">
