1131fb9b1fad27b0114685d704060ff1a0248319
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-13 21:07:28 +0200

    fix: showing all text

--- resources/js/components/subpages/manage/ManageSubPage.vue
+++ resources/js/components/subpages/manage/ManageSubPage.vue
@@ -10,7 +10,7 @@
     <Pagination :elements="usersProp.meta" v-if="!isSearching" />
     <p
         class="text-sm text-zinc-300 border-t border-zinc-800 py-3"
-        v-if="usersProp.meta.total <= usersProp.meta.per_page"
+        v-if="usersProp.meta.total <= usersProp.meta.per_page && !isSearching"
     >
         Showing all results
     </p>
