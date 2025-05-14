aa70f8bf72346ad3f240767383c9710c61d1ec60
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-14 23:36:57 +0200

    using heroicons now for play/pause

--- resources/js/components/subpages/voting/VotingScreen.vue
+++ resources/js/components/subpages/voting/VotingScreen.vue
@@ -29,42 +29,25 @@
             @touchend.passive="endDrag"
         >
             <!-- Audio play button overlay -->
-            <div
-                class="absolute top-4 right-4 z-20"
-                v-if="swipeLengthX >= -15 && swipeLengthX <= 15"
-            >
-                <button
-                    @click.stop="toggleAudio(song.track_id)"
-                    class="w-12 h-12 bg-primary bg-opacity-50 rounded-full flex items-center justify-center hover:bg-opacity-70 transition-all"
+            <transition name="fade">
+                <div
+                    class="absolute top-4 right-4 z-20"
+                    v-if="swipeLengthX >= -15 && swipeLengthX <= 15"
                 >
-                    <svg
-                        v-if="currentPlayingId !== song.track_id || !isPlaying"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="white"
-                        class="w-6 h-6"
+                    <button
+                        @click.stop="toggleAudio(song.track_id)"
+                        class="w-12 h-12 bg-primary bg-opacity-50 rounded-full flex items-center justify-center hover:bg-opacity-70 transition-all cursor-pointer"
                     >
-                        <path
-                            fill-rule="evenodd"
-                            d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-                            clip-rule="evenodd"
+                        <PlayIcon
+                            v-if="
+                                currentPlayingId !== song.track_id || !isPlaying
+                            "
+                            class="w-6 h-6 text-white"
                         />
-                    </svg>
-                    <svg
-                        v-else
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="white"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                </button>
-            </div>
+                        <PauseIcon v-else class="w-6 h-6 text-white" />
+                    </button>
+                </div>
+            </transition>
 
             <div class="gradient-bg"></div>
             <div
@@ -169,7 +152,8 @@
 </template>
 
 <script setup>
-import { HeartIcon, XMarkIcon } from "@heroicons/vue/24/solid";
+import { PauseIcon } from "@heroicons/vue/24/solid";
+import { HeartIcon, PlayIcon, XMarkIcon } from "@heroicons/vue/24/solid";
 import axios from "axios";
 import { ref, computed, onMounted, watch } from "vue";
 
