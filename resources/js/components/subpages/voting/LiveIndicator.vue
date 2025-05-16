24748e503326cf9de0869e890d655b1d280b0188
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-17 01:30:17 +0200

    feat: added live indicator + random hardcoded dislike/like count

---
+++ resources/js/components/subpages/voting/LiveIndicator.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="relative w-4 h-4">
+        <div
+            class="absolute inset-0 rounded-full animate-ping"
+            :class="{
+                'bg-red-400': isLive,
+                'bg-gray-400': !isLive,
+            }"
+        ></div>
+        <div
+            class="w-4 h-4 rounded-full z-10"
+            :class="{
+                'bg-red-400': isLive,
+                'bg-gray-400': !isLive,
+            }"
+        ></div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    isLive: {
+        type: Boolean,
+        required: true,
+    },
+});
+</script>
--- resources/js/components/subpages/voting/VotingSubPage.vue
+++ resources/js/components/subpages/voting/VotingSubPage.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="flex flex-row items-center gap-6 mb-6 lg:mb-8">
-        <h1 class="text-4xl sm:text-5xl font-medium mb-2">Live leaderboard</h1>
+    <div class="flex flex-row items-center mb-6 lg:mb-8">
+        <LiveIndicator :isLive="true" class="mb-2 mr-4" />
+        <h1 class="text-4xl sm:text-5xl font-medium mb-2">Live Rankings</h1>
         <RegularButton
             color="blue"
             @click="isVisible = true"
-            external-class="hidden lg:inline-flex mb-2"
+            external-class="hidden lg:inline-flex mb-2 ml-6"
         >
             Vote now!
         </RegularButton>
@@ -49,7 +50,12 @@
                                     d="M10 19c-.072 0-.145 0-.218-.006A4.1 4.1 0 0 1 6 14.816V11H2.862a1.751 1.751 0 0 1-1.234-2.993L9.41.28a.836.836 0 0 1 1.18 0l7.782 7.727A1.751 1.751 0 0 1 17.139 11H14v3.882a4.134 4.134 0 0 1-.854 2.592A3.99 3.99 0 0 1 10 19Zm0-17.193L2.685 9.071a.251.251 0 0 0 .177.429H7.5v5.316A2.63 2.63 0 0 0 9.864 17.5a2.441 2.441 0 0 0 1.856-.682A2.478 2.478 0 0 0 12.5 15V9.5h4.639a.25.25 0 0 0 .176-.429L10 1.807Z"
                                 ></path>
                             </svg>
-                            <p class="text-xs text-zinc-400">5</p>
+                            <p class="text-xs text-zinc-400">
+                                {{
+                                    Math.floor(Math.random() * (10 - -5 + 1)) +
+                                    -5
+                                }}
+                            </p>
                             <svg
                                 rpl=""
                                 fill="currentColor"
@@ -95,6 +101,7 @@ import { computed, ref } from "vue";
 import RegularButton from "@/components/buttons/RegularButton.vue";
 import VotingFullScreen from "@/components/subpages/voting/VotingFullScreen.vue";
 import { usePage } from "@inertiajs/vue3";
+import LiveIndicator from "@/components/subpages/voting/LiveIndicator.vue";
 import { HandThumbDownIcon, HandThumbUpIcon } from "@heroicons/vue/24/outline";
 
 const page = usePage();
