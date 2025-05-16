269abbae4c5567deb0f5c11796011be2f0b21d0b
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-16 02:06:25 +0200

    feat: changes to mobile navbar

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -79,7 +79,7 @@
 
         <!-- top part of navigation mobile-->
         <div
-            class="sticky top-0 z-40 flex items-center gap-x-6 bg-navbar-background px-2 py-2 shadow-xs sm:px-4 lg:hidden border-b-2 border-regular-stroke"
+            class="sticky top-0 z-40 flex items-center gap-x-6 bg-navbar-background/75 backdrop-blur-md px-2 py-2 shadow-xs sm:px-4 lg:hidden border-b-2 border-regular-stroke"
         >
             <button
                 type="button"
