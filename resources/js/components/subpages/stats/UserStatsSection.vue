8174d441ef073921641bbe49a1e41fe7e3538f39
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-20 01:13:41 +0200

    margin fixes

--- resources/js/components/subpages/stats/UserStatsSection.vue
+++ resources/js/components/subpages/stats/UserStatsSection.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="mb-16">
-        <div class="flex flex-row items-center mb-6 lg:mb-8">
-            <h1 class="text-3xl sm:text-4xl font-medium mb-2">User facts</h1>
+        <div class="flex flex-row items-center mb-2">
+            <h1 class="text-3xl sm:text-4xl font-medium">User facts</h1>
             <RegularButton color="blue" external-class="inline-flex mb-2 ml-6"
                 >Refresh cards</RegularButton
             >
