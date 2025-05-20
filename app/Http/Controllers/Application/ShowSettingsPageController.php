4f3ebf77d46f69e112b243a687a8f997161492fa
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-20 23:36:53 +0200

    feat: added "delete account" and "accessibility" section

--- app/Http/Controllers/Application/ShowSettingsPageController.php
+++ app/Http/Controllers/Application/ShowSettingsPageController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers\Application;
 use App\Http\Controllers\Controller;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Support\Facades\Auth;
+
 
 class ShowSettingsPageController extends Controller
 {
     public function __invoke(): Response
     {
-        return Inertia::render('UserSettingsPage');
+        $user = Auth::user();
+        return Inertia::render('UserSettingsPage', [
+            'your_mixes' => fn() => $user->mixes,
+            'joined_mixes' => fn() => $user->accessibleMixes,
+        ]);
     }
 }
--- resources/js/pages/UserSettingsPage.vue
+++ resources/js/pages/UserSettingsPage.vue
@@ -1,7 +1,58 @@
 <template>
-    <div></div>
+    <Head title="Tunofy | App" />
+    <AppLayout>
+        <Transition name="fade-with-slide" appear mode="out-in">
+            <div class="flex flex-col gap-8 max-w-3xl">
+                <section class="mb-8">
+                    <div class="mb-8 py-6 border-b-2 border-card-stroke">
+                        <h1 class="text-3xl sm:text-4xl font-medium mb-2">
+                            Settings
+                        </h1>
+                        <p class="text-muted">
+                            Manage your profile and account settings
+                        </p>
+                    </div>
+                    <div class="mb-4 space-y-1">
+                        <h2 class="text-xl sm:text-2xl">Delete account</h2>
+                        <p class="text-muted">
+                            Delete your account and all of its resources
+                        </p>
+                    </div>
+                    <div
+                        class="space-y-4 rounded-lg border border-red-100 bg-red-50 p-4 dark:border-red-200/10 dark:bg-red-700/10"
+                    >
+                        <div
+                            class="relative space-y-1.5 text-red-600 dark:text-red-100"
+                        >
+                            <p class="font-medium text-lg">Warning</p>
+                            <p class="text-sm">
+                                Please proceed with caution, this cannot be
+                                undone.
+                            </p>
+                        </div>
+                        <RegularButton type="danger" external-class="mt-2">
+                            Delete account
+                        </RegularButton>
+                    </div>
+                </section>
+                <section class="mb-8">
+                    <div class="mb-4 space-y-1">
+                        <h2 class="text-xl sm:text-2xl">
+                            Accessibility (coming soon)
+                        </h2>
+                        <p class="text-muted">
+                            Manage accessibility settings for your account
+                        </p>
+                    </div>
+                </section>
+            </div>
+        </Transition>
+    </AppLayout>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<script setup>
+import { ref } from "vue";
+import AppLayout from "@/layouts/AppLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import RegularButton from "@/components/buttons/RegularButton.vue";
+</script>
