692a90de02778dc9a12875b3d5983086c1f6340d
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-05-21 01:09:13 +0200

    ui changes

--- resources/js/components/main-page/NewsSection.vue
+++ resources/js/components/main-page/NewsSection.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="mb-16">
-        <h1 class="text-3xl sm:text-4xl font-medium mb-2">Latest News</h1>
-        <p class="mb-8 text-zinc-400">
+        <h1 class="text-3xl sm:text-4xl font-medium">Latest News</h1>
+        <p class="mb-8 text-muted">
             Stay updated with the latest versions, teasers, and announcements.
         </p>
 
