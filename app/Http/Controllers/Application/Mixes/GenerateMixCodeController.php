0d2601b94004e1f8790060ac21508f9cf0c1c8a7
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-06-02 01:12:17 +0200

    fix: error session name in GenerateMixCodeController.php

--- app/Http/Controllers/Application/Mixes/GenerateMixCodeController.php
+++ app/Http/Controllers/Application/Mixes/GenerateMixCodeController.php
@@ -15,10 +15,11 @@ class GenerateMixCodeController extends Controller
     {
         $validated = $request->validated();
 
+
         try {
             $normalCode = Str::random(5);
             $code = Str::upper($normalCode);
-            
+
             $qrCode = QrCode::size(300)->generate(
                 route('mix.join', ['session_code' => $code])
             );
@@ -30,17 +31,17 @@ class GenerateMixCodeController extends Controller
                 'session_code_expires_at' => now()->addMinutes(30),
             ]);
 
-
             $qrCodeBase64 = base64_encode($qrCode);
 
             return redirect()->back()
                 ->with([
                     'session_code' => $mix->session_code,
-                    'qr_code' => $qrCodeBase64
+                    'qr_code' => $qrCodeBase64,
                 ]);
         } catch (\Exception $e) {
+            \Log::error("Failed to generate mix code: " . $e->getMessage());
             return redirect()->back()
-                ->with('danger', 'Failed to generate mix code');
+                ->with('error', 'Failed to generate mix code');
         }
     }
 }
