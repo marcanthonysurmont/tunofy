7e68b331528033f73a2411ac045b86bbd30ebddf
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2025-06-03 15:36:47 +0200

    vite config changes to include landingpage.js in build

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input:['resources/js/app.js', 'resources/js/landingpage.js'],
             refresh: true,
         }),
         tailwindcss(),
