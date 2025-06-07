985e9df2874f8eae29292d326cd9cc5d4abf6c9a
marcanthonysurmont <147101133+marcanthonysurmont@users.noreply.github.com>
2025-06-07 17:35:07 +0200

    Remove GenreStatsSection from StatsSubPage.vue

--- resources/js/components/subpages/stats/StatsSubPage.vue
+++ resources/js/components/subpages/stats/StatsSubPage.vue
@@ -1,11 +1,9 @@
 <template>
     <FunStatsSection />
-    <GenreStatsSection />
     <UserStatsSection />
 </template>
 
 <script setup>
 import FunStatsSection from "@/components/subpages/stats/FunStatsSection.vue";
-import GenreStatsSection from "@/components/subpages/stats/GenreStatsSection.vue";
 import UserStatsSection from "@/components/subpages/stats/UserStatsSection.vue";
 </script>
