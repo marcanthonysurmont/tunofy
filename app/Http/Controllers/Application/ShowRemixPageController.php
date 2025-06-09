b9191691f6d21c0ff84e023aebc08334edf7c840
marcanthonysurmont <147101133+marcanthonysurmont@users.noreply.github.com>
2025-06-09 17:16:43 +0200

    Refactor method to get highest added song in UserSongHistory

--- app/Http/Controllers/Application/ShowRemixPageController.php
+++ app/Http/Controllers/Application/ShowRemixPageController.php
@@ -14,9 +14,7 @@ class ShowRemixPageController extends Controller
     {
         $user = Auth::user();
 
-        $highestAddedSong = UserSongHistory::where('user_id', $user->id)
-            ->orderBy('times_added', 'desc')
-            ->first();
+        $highestAddedSong = UserSongHistory::highestAddedSong($user);
 
         return Inertia::render('RemixPage',[
             'globalUserStat' => $user->globalUserStat,
